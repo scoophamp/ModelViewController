6b2c2ed4cea396e559dd0368d8e3dd68dfeb2640
scoophamp <hampus_cousin@hotmailcom>
2017-02-06 15:12:56 +0100

    More entity

--- MVC.Data/MVC.Data/Comment.cs
+++ MVC.Data/MVC.Data/Comment.cs
@@ -8,5 +8,16 @@ namespace MVC.Data
 {
    public class Comment
     {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Guid UserID { get; set; }
+        public virtual User User { get; set; }
+        public Guid? PhotoID { get; set; }
+        public virtual Photo Photo { get; set; }
+        public Guid? AlbumId { get; set; }
+        public virtual Album Album { get; set; }
     }
 }
--- MVC.Data/MVC.Data/Photo.cs
+++ MVC.Data/MVC.Data/Photo.cs
@@ -8,5 +8,21 @@ namespace MVC.Data
 {
    public class Photo
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Guid UserID { get; set; }
+        public virtual User User { get; set; }
+        public Guid? AlbumID { get; set; }
+        public virtual Album Album { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+
+        public Photo()
+        {
+            Comments = new HashSet<Comment>();
+            Id = Guid.NewGuid();
+        }
     }
 }
